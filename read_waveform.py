7bf16117f27c53d7bf6ee60b4493f0ed57179b30
eneights <eun24@psu.edu>
2019-05-15 15:18:33 -0400

    read_waveform calculates time of half max

--- read_waveform.py
+++ read_waveform.py
@@ -1,4 +1,3 @@
-import sys
 import os
 import numpy as np
 import matplotlib.pyplot as plt
@@ -9,30 +8,40 @@ path = Path(r'/Users/Eliza/Documents/WATCHMAN/20190514_watchman_spe/')
 cwd = os.getcwd()
 os.chdir(path)
 
-
 def read_waveform(file_name, nhdr):
-    myfile = open(file_name, 'rb')
-    header = []
-    x = np.array([])
-    y = np.array([])
+    myfile = open(file_name, 'rb')          # opens file
+    header = []                             # creates empty list for header
+    x = np.array([])                        # creates empty array for time
+    y = np.array([])                        # creates empty array for voltage
     for i in range(nhdr):
-        header.append(myfile.readline())
+        header.append(myfile.readline())    # reads header and adds each line to header list
+        header_string = '\n'.join(a.decode("cp1250") for a in header)   # creates string out of header
     for line in myfile:
-        x = np.append(x, float(line.split(str.encode(','))[0]))
-        y = np.append(y, float(line.split(str.encode(','))[1]))
+        x = np.append(x, float(line.split(str.encode(','))[0]))         # fills array with times from file
+        y = np.append(y, float(line.split(str.encode(','))[1]))         # fills array with voltages from file
+    half_max_value = min(y) / 2
+    xvals = np.linspace(4.64e-7, 6.64e-7, int(2e6))
+    yvals = np.interp(xvals, x, y)
+    differential = np.diff(yvals)
+    difference_value = np.abs(yvals - half_max_value)
+    for i in range(0, len(differential)):
+        if differential[i] > 0:
+            difference_value[i] = np.inf
+    index = np.argmin(difference_value)
+    time = xvals[index]
     myfile.close()
-    return x, y, header
+    return x, y, header_string, half_max_value, time
 
 
 if __name__ == '__main__':
     import argparse
     parser = argparse.ArgumentParser(prog="read waveform", description="read the waveform datafile.")
     parser.add_argument("--nhdr", type=int, help='number of header lines to skip in the raw file', default=5)
-    parser.add_argument("--file_name", type=str,help="filename", default="./C2--waveforms--00000.txt")
+    parser.add_argument("--file_name", type=str, help="filename", default="./C2--waveforms--00030.txt")
     args = parser.parse_args()
 
-    t, v, hdr = read_waveform(args.file_name, args.nhdr)
-    print(hdr)
+    t, v, hdr, half_max, half_max_time = read_waveform(args.file_name, args.nhdr)
+    print("\nHeader:\n\n", hdr)
     plt.plot(t, v)
     plt.show()
 
--- write_waveform.py
+++ write_waveform.py
@@ -1,30 +1,35 @@
 import sys
+import os
 import numpy as np
 import matplotlib.pyplot as plt
-import os
+import datetime as dt
 from pathlib import Path
+from read_waveform import read_waveform as rw
 
 # path = Path(r'/Volumes/TOSHIBA EXT/data/watchman/20190513_watchman_spe/waveforms/200MHz_bandwidth/')
 path = Path(r'/Users/Eliza/Documents/WATCHMAN/20190514_watchman_spe/')
 cwd = os.getcwd()
 os.chdir(path)
 
+header_default = "LECROYWaveRunner‘N,20113,Waveform\nSegments,1,SegmentSize,4002\n#1," + str(dt.date.today()) + ",0\nTime,Ampl"
 
 def write_waveform(x, y, file_name, hdr):
-    fout = open(file_name, 'w')
+    fout = open(file_name, 'w')             # opens file
     for entry in str(hdr):
-        fout.write(entry)
+        fout.write(entry)                   # writes header in file
     for ix, iy in zip(x, y):
-        line = '%.7E,%f\n' % (ix, iy)
-        fout.write(line)
+        line = '%.7E,%f\n' % (ix, iy)       # formats line
+        fout.write(line)                    # writes line in file
     fout.close()
+
 
 if __name__ == '__main__':
     import argparse
     parser = argparse.ArgumentParser(prog="write waveform", description="write a waveform datafile.")
-    parser.add_argument("--hdr", type=str, help='header string for the output file', default=5)
+    parser.add_argument("--hdr", type=str, help='header string for the output file', default=header_default)
     parser.add_argument("--file_name", type=str, help="filename", default="./C2--waveforms--00000.txt")
     args = parser.parse_args()
+
     x = range(1000)
     y = [np.random.normal(0, 1.) for i in range(1000)]
     write_waveform(x, y, args.file_name, args.hdr)
