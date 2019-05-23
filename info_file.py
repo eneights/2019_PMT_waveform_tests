fc7e08de0a5b4602383a2e219807418b4be6a9a8
eneights <eun24@psu.edu>
2019-05-23 11:27:33 -0400

    code for creating info file in info_file.py

--- info_file.py
+++ info_file.py
@@ -8,17 +8,17 @@ def info_file(acq_date_time, source_path, dest_path, pmt_hv, gain, offset, trig_
     file_name = 'info.txt'
     file = dest_path / file_name
     myfile = open(file, 'w')
-    myfile.write('Data acquisition,' + str(acq_date_time))
-    myfile.write('\nData processing,' + str(now))
-    myfile.write('\nSource data,' + str(source_path))
-    myfile.write('\nDestination data,' + str(dest_path))
-    myfile.write('\nPMT HV (V),' + str(pmt_hv))
-    myfile.write('\nNominal gain,' + str(gain))
-    myfile.write('\nDG 535 offset,' + str(offset))
-    myfile.write('\nDG 535 trigger delay (ns),' + str(trig_delay))
-    myfile.write('\nDG 535 amplitude (V),' + str(amp))
-    myfile.write('\nOscilloscope sample rate (Hz),' + str(fsps))
-    myfile.write('\nOscilloscope bandwidth (Hz),' + str(band))
-    myfile.write('\nOscilloscope noise filter (bits),' + str(nfilter))
-    myfile.write('\nOscilloscope resistance (ohms),' + str(r))
+    myfile.write('Data acquisition,' + str(acq_date_time))              # date & time of raw data from file header
+    myfile.write('\nData processing,' + str(now))                       # current date & time
+    myfile.write('\nSource data,' + str(source_path))                   # path to raw data
+    myfile.write('\nDestination data,' + str(dest_path))                # path to 50% max and baseline shifted data
+    myfile.write('\nPMT HV (V),' + str(pmt_hv))                         # voltage of PMT from d0 info file
+    myfile.write('\nNominal gain,' + str(gain))                         # gain of PMT from d0 info file
+    myfile.write('\nDG 535 offset,' + str(offset))                      # offset of pulse generator from d0 info file
+    myfile.write('\nDG 535 trigger delay (ns),' + str(trig_delay))  # trigger delay of pulse generator from d0 info file
+    myfile.write('\nDG 535 amplitude (V),' + str(amp))                  # amplitude of pulse generator from d0 info file
+    myfile.write('\nOscilloscope sample rate (Hz),' + str(fsps))        # sample rate of oscilloscope from d0 info file
+    myfile.write('\nOscilloscope bandwidth (Hz),' + str(band))          # bandwidth of oscilloscope from d0 info file
+    myfile.write('\nOscilloscope noise filter (bits),' + str(nfilter))  # oscilloscope noise filter from d0 info file
+    myfile.write('\nOscilloscope resistance (ohms),' + str(r))          # resistance of oscilloscope from d0 info file
     myfile.close()
