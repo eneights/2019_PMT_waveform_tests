879d7493235d050a7425b02e3ca2ec03061538f0
eneights <eun24@psu.edu>
2019-06-03 13:18:10 -0400

    created working p2

--- p2_/functions.py
+++ p2_/functions.py
@@ -6,6 +6,7 @@ from pathlib import Path
 from scipy.optimize import curve_fit
 from scipy.stats import norm
 
+
 # Reads csv file with header and time & voltage columns
 # Returns time array, voltage array, and header as a string
 def rw(file_name, nhdr):
@@ -29,3 +30,212 @@ def rw(file_name, nhdr):
             header_str += head_str
 
     return x, y, header_str
+
+
+# Given a time array, voltage array, and header, writes a csv file with header and time & voltage columns
+def ww(x, y, file_name, hdr):
+    myfile = open(file_name, 'w')           # Opens file to write waveform into
+    for entry in str(hdr):                  # Writes header to file
+        myfile.write(entry)
+    for ix, iy in zip(x, y):                # Writes time and voltage values into file
+        line = '%.7E,%f\n' % (ix, iy)
+        myfile.write(line)
+    myfile.close()                          # Closes waveform file
+
+
+# Calculates the average waveform of an spe
+def average_waveform(start, end, data_file, dest_path, nhdr, save_name):
+    save_file = Path(dest_path / 'plots')
+    tsum = 0
+    vsum = 0
+    n = 0
+    for i in range(start, end + 1):
+        file_name = 'D2--waveforms--%05d.txt' % i
+        if os.path.isfile(data_file / file_name):
+            print('Reading file #', i)
+            t, v, hdr = rw(data_file / file_name, nhdr)     # Reads a waveform file
+            v = v / min(v)                                  # Normalizes voltages
+            idx = np.where(t == 0)                          # Finds index of t = 0 point
+            idx = int(idx[0])
+            t = np.roll(t, -idx)                            # Rolls time array so that t = 0 point is at index 0
+            v = np.roll(v, -idx)                            # Rolls voltage array so that 50% max point is at index 0
+            idx2 = np.where(t == min(t))                    # Finds index of point of minimum t
+            idx2 = int(idx2[0])
+            idx3 = np.where(t == max(t))                    # Finds index of point of maximum t
+            idx3 = int(idx3[0])
+            # Only averages waveform files that have enough points before t = 0 & after the spe
+            if idx2 <= 3430:
+                # Removes points between point of maximum t & chosen minimum t in time & voltage arrays
+                t = np.concatenate((t[:idx3], t[3430:]))
+                v = np.concatenate((v[:idx3], v[3430:]))
+                # Rolls time & voltage arrays so that point of chosen minimum t is at index 0
+                t = np.roll(t, -idx3)
+                v = np.roll(v, -idx3)
+                if len(t) >= 3920:
+                    # Removes points after chosen point of maximum t in time & voltage arrays
+                    t = t[:3920]
+                    v = v[:3920]
+                    # Sums time & voltage arrays
+                    tsum += t
+                    vsum += v
+                    n += 1
+    # Finds average time & voltage arrays
+    t_avg = tsum / n
+    v_avg = vsum / n
+
+    # Plots average waveform & saves image
+    plt.plot(t_avg, v_avg)
+    plt.xlabel('Time (s)')
+    plt.ylabel('Normalized Voltage')
+    plt.title('Average Waveform')
+    plt.savefig(save_file / (save_name + '.png'), dpi=360)
+    # plt.show()
+
+    # Saves average waveform data
+    file_name = save_file / (save_name + '.txt')
+    hdr = 'Average Waveform\n\n\n\nTime,Ampl'
+    ww(t_avg, v_avg, file_name, hdr)
+
+
+def lowpass_filter(v, tau, fsps):
+    alpha = 1 - np.exp(-1. / (fsps * tau))
+    v_filtered = np.array([])
+    for i in range(len(v)):
+        if i == 0:
+            v_filtered = np.append(v_filtered, v[i])
+        else:
+            v2 = v[i] * alpha + (1 - alpha) * v_filtered[i - 1]
+            v_filtered = np.append(v_filtered, v2)
+    return v_filtered
+
+
+# Returns the average baseline (baseline noise level)
+def calculate_average(t, v):
+    v_sum = 0
+
+    idx = np.where(v == min(v))     # Finds index of point of minimum voltage value
+    idx = idx[0]
+
+    if idx > len(t) / 2:            # If minimum voltage is in second half of voltage array, calculates baseline using
+        idx1 = int(.1 * len(t))     # first half of voltage array
+        idx2 = int(.35 * len(t))
+    else:
+        idx1 = int(.65 * len(t))    # If minimum voltage is in first half of voltage array, calculates baseline using
+        idx2 = int(.9 * len(t))     # second half of voltage array
+    for i in range(idx1, idx2):
+        v_sum += v[i]
+    average = v_sum / (idx2 - idx1)
+
+    return average
+
+
+def calculate_charge(t, v, r):
+    vsum = 0
+    idx1 = np.inf
+    idx2 = np.inf
+
+    min_val = min(v)                        # Finds minimum voltage value
+    idx_min_val = np.where(v == min_val)    # Finds index of minimum voltage value in voltage array
+    time_min_val = t[idx_min_val]           # Finds time of point of minimum voltage
+    min_time = time_min_val[0]
+
+    tvals = np.linspace(t[0], t[len(t) - 1], 5000)      # Creates array of times over entire timespan
+    tvals1 = np.linspace(t[0], min_time, 5000)          # Creates array of times from beginning to point of min voltage
+    tvals2 = np.linspace(min_time, t[len(t) - 1], 5000)  # Creates array of times from point of min voltage to end
+    vvals = np.interp(tvals, t, v)     # Interpolates & creates array of voltages over entire timespan
+    vvals1 = np.interp(tvals1, t, v)   # Interpolates & creates array of voltages from beginning to point of min voltage
+    vvals2 = np.interp(tvals2, t, v)   # Interpolates & creates array of voltages from point of min voltage to end
+    vvals1_flip = np.flip(vvals1)      # Flips array, creating array of voltages from point of min voltage to beginning
+    difference_value1 = vvals1_flip - (0.1 * min_val)   # Finds difference between points in beginning array and 10% max
+    difference_value2 = vvals2 - (0.1 * min_val)        # Finds difference between points in end array and 10% max
+
+    for i in range(0, len(difference_value1) - 1):  # Starting at point of minimum voltage and going towards beginning
+        if difference_value1[i] >= 0:               # of waveform, finds where voltage becomes greater than 10% max
+            idx1 = len(difference_value1) - i
+            break
+    if idx1 == np.inf:      # If voltage never becomes greater than 10% max, finds where voltage is closest to 10% max
+        idx1 = len(difference_value1) - 1 - np.argmin(np.abs(difference_value1))
+    for i in range(0, len(difference_value2) - 1):  # Starting at point of minimum voltage and going towards end of
+        if difference_value2[i] >= 0:               # waveform, finds where voltage becomes greater than 10% max
+            idx2 = i
+            break
+    if idx2 == np.inf:      # If voltage never becomes greater than 10% max, finds where voltage is closest to 10% max
+        idx2 = np.argmin(np.abs(difference_value2))
+
+    diff_val1 = np.abs(tvals - tvals1[idx1])        # Finds differences between values in time array and times when
+    diff_val2 = np.abs(tvals - tvals2[idx2])        # spe starts and ends
+    index1 = np.argmin(diff_val1)       # Finds index of beginning of spe
+    index2 = np.argmin(diff_val2)       # Finds index of end of spe
+    t1 = tvals[index1]      # Finds time of beginning of spe
+    t2 = tvals[index2]      # Finds time of end of spe
+    for i in range(len(tvals)):         # Calculates sum of all voltages in full timespan
+        vsum += vvals[i]
+    charge = -1 * (tvals[len(tvals) - 1]) * vsum / (len(tvals) * r)     # Calculates charge
+
+    return t1, t2, charge
+
+
+# Returns 10-90 and 20-80 rise times
+def rise_time(t, v, r):
+    t1, t2, charge = calculate_charge(t, v, r)      # Calculates start time of spe
+    idx_min_val = np.where(v == min(v))     # Finds index of minimum voltage
+    time_min_val = t[idx_min_val]           # Finds time at point of minimum voltage
+    min_time = time_min_val[0]
+
+    val10 = .1 * (min(v))           # Calculates 10% max
+    val20 = 2 * val10               # Calculates 20% max
+    val80 = 8 * val10               # Calculates 80% max
+    val90 = 9 * val10               # Calculates 90% max
+    tvals = np.linspace(t1, min_time, 5000)   # Creates array of times from beginning of spe to point of minimum voltage
+    vvals = np.interp(tvals, t, v)   # Interpolates & creates array of voltages from beginning of spe to minimum voltage
+    difference_value10 = np.abs(vvals - val10)      # Calculates difference between values in voltage array and 10% max
+    difference_value20 = np.abs(vvals - val20)      # Calculates difference between values in voltage array and 20% max
+    difference_value80 = np.abs(vvals - val80)      # Calculates difference between values in voltage array and 80% max
+    difference_value90 = np.abs(vvals - val90)      # Calculates difference between values in voltage array and 90% max
+    index10 = np.argmin(difference_value10)     # Finds index of point of 10% max
+    index20 = np.argmin(difference_value20)     # Finds index of point of 20% max
+    index80 = np.argmin(difference_value80)     # Finds index of point of 80% max
+    index90 = np.argmin(difference_value90)     # Finds index of point of 90% max
+    time10 = tvals[index10]         # Finds time of point of 10% max
+    time20 = tvals[index20]         # Finds time of point of 20% max
+    time80 = tvals[index80]         # Finds time of point of 80% max
+    time90 = tvals[index90]         # Finds time of point of 90% max
+    rise_time1090 = time90 - time10     # Calculates 10-90 rise time
+    rise_time2080 = time80 - time20     # Calculates 20-80 rise time
+    rise_time1090 = float(format(rise_time1090, '.2e'))
+    rise_time2080 = float(format(rise_time2080, '.2e'))
+
+    return rise_time1090, rise_time2080
+
+
+def rise_time_1090(t, v):
+    idx = np.inf
+    idx_min_val = np.where(v == min(v))     # Finds index of minimum voltage value in voltage array
+    time_min_val = t[idx_min_val]           # Finds time of point of minimum voltage
+    min_time = time_min_val[0]
+
+    tvals = np.linspace(t[0], t[len(t) - 1], 5000)  # Creates array of times over entire timespan
+    tvals1 = np.linspace(t[0], min_time, 5000)      # Creates array of times from beginning to point of min voltage
+    vvals = np.interp(tvals, t, v)                  # Interpolates & creates array of voltages over entire timespan
+    vvals1 = np.interp(tvals1, t, v)   # Interpolates & creates array of voltages from beginning to point of min voltage
+    vvals1_flip = np.flip(vvals1)       # Flips array, creating array of voltages from point of min voltage to beginning
+    difference_value = vvals1_flip - (0.1 * min(v))    # Finds difference between points in beginning array and 10% max
+
+    for i in range(0, len(difference_value) - 1):  # Starting at point of minimum voltage and going towards beginning
+        if difference_value[i] >= 0:               # of waveform, finds where voltage becomes greater than 10% max
+            idx = len(difference_value) - i
+            break
+    if idx == np.inf:       # If voltage never becomes greater than 10% max, finds where voltage is closest to 10% max
+        idx = len(difference_value) - 1 - np.argmin(np.abs(difference_value))
+    t1 = tvals[np.argmin(np.abs(tvals - tvals1[idx]))]  # Finds time of beginning of spe
+
+    val10 = .1 * (min(v))   # Calculates 10% max
+    val90 = 9 * val10       # Calculates 90% max
+    tvals2 = np.linspace(t1, min_time, 5000)  # Creates array of times from beginning of spe to point of minimum voltage
+    vvals2 = np.interp(tvals2, t, v)     # Interpolates & creates array of voltages from beginning of spe to min voltage
+
+    time10 = tvals[np.argmin(np.abs(vvals2 - val10))]  # Finds time of point of 10% max
+    time90 = tvals[np.argmin(np.abs(vvals2 - val90))]  # Finds time of point of 90% max
+    rise_time1090 = float(format(time90 - time10, '.2e'))       # Calculates 10-90 rise time
+
+    return rise_time1090
--- p2_/p2.py
+++ p2_/p2.py
@@ -1,18 +1,111 @@
-from scipy import signal
 from functions import *
+from info_file import info_file
 
 
-def p2(start, end, numtaps, fc, fsps, nhdr):
+def p2(start, end, date, date_time, filter_band, nhdr, fsps, fc, numtaps, r, pmt_hv, gain, offset, trig_delay, amp,
+       band, nfilter):
+    gen_path = Path(r'/Volumes/TOSHIBA EXT/data/watchman')
+    save_path = Path(str(gen_path / '%08d_watchman_spe/waveforms/%s') % (date, filter_band))
+    data_path = Path(save_path / 'd1')
+    dest_path = Path(save_path / 'd2')
+    filt_path1 = Path(dest_path / 'filter1')
+    filt_path2 = Path(dest_path / 'filter2')
+    filt_path4 = Path(dest_path / 'filter4')
+    filt_path8 = Path(dest_path / 'filter8')
 
-    lowpass1 = signal.firwin(numtaps, cutoff=2. * fc / fsps, window='blackman')
-    lowpass2 = signal.firwin(numtaps, cutoff=fc / fsps, window='blackman')
-    lowpass4 = signal.firwin(numtaps, cutoff=fc / (2. * fsps), window='blackman')
-    lowpass8 = signal.firwin(numtaps, cutoff=fc / (4. * fsps), window='blackman')
+    x1_array = np.array([])
+    j_array = np.array([])
 
-    file_name =
+    for i in range(start, end + 1):
+        file_name = str(data_path / 'd1_shifted' / 'D1--waveforms--%05d.txt') % i
+        save_name1 = str(filt_path1 / 'D2--waveforms--%05d.txt') % i
+        save_name2 = str(filt_path2 / 'D2--waveforms--%05d.txt') % i
+        save_name4 = str(filt_path4 / 'D2--waveforms--%05d.txt') % i
+        save_name8 = str(filt_path8 / 'D2--waveforms--%05d.txt') % i
 
-    t, v, hdr = rw(file_name, nhdr)
-    v1 = signal.filtfilt(lowpass1, 1.0, v)
-    v2 = signal.filtfilt(lowpass2, 1.0, v)
-    v4 = signal.filtfilt(lowpass4, 1.0, v)
-    v8 = signal.filtfilt(lowpass8, 1.0, v)
+        if os.path.isfile(file_name):
+            print('Filtering file #%05d' % i)
+            t, v, hdr = rw(file_name, nhdr)
+            rt1090, rt2080 = rise_time(t, v, nhdr)
+            for k in range(5, 3000):
+                j = k * 2e-10
+                v_new = lowpass_filter(v, j, fsps)
+                x1 = rise_time_1090(t, v_new)
+                x1_array = np.append(x1_array, x1)
+                j_array = np.append(j_array, j)
+            tau_2 = j_array[np.argmin(np.abs(x1_array - 2 * rt1090))]
+            tau_4 = j_array[np.argmin(np.abs(x1_array - 4 * rt1090))]
+            tau_8 = j_array[np.argmin(np.abs(x1_array - 8 * rt1090))]
+            v2 = lowpass_filter(v, tau_2, fsps)
+            v4 = lowpass_filter(v, tau_4, fsps)
+            v8 = lowpass_filter(v, tau_8, fsps)
+            ww(t, v, save_name1, hdr)
+            ww(t, v2, save_name2, hdr)
+            ww(t, v4, save_name4, hdr)
+            ww(t, v8, save_name8, hdr)
+
+    average_waveform(start, end, filt_path1, dest_path, nhdr, 'avg_waveform1')
+    average_waveform(start, end, filt_path2, dest_path, nhdr, 'avg_waveform2')
+    average_waveform(start, end, filt_path4, dest_path, nhdr, 'avg_waveform4')
+    average_waveform(start, end, filt_path8, dest_path, nhdr, 'avg_waveform8')
+
+    info_file(date_time, data_path, dest_path, pmt_hv, gain, offset, trig_delay, amp, fsps, band, nfilter, r)
+
+
+if __name__ == '__main__':
+    import argparse
+    parser = argparse.ArgumentParser(prog="p2", description="Creating D2")
+    parser.add_argument("--start", type=int, help='file number to begin at (default=0)', default=0)
+    parser.add_argument("--end", type=int, help='file number to end at (default=99999)', default=99999)
+    parser.add_argument("--date", type=int, help='date of data acquisition (YEARMMDD)')
+    parser.add_argument("--date_time", type=str, help='date & time of d1 processing')
+    parser.add_argument("--fil_band", type=str, help='folder name for data')
+    parser.add_argument("--nhdr", type=int, help='number of header lines to skip in raw file (default=5)', default=5)
+    parser.add_argument("--fsps", type=float, help='samples per second (Hz) (suggested=20000000000.)')
+    parser.add_argument("--fc", type=float, help='filter cutoff frequency (Hz) (default=250000000)', default=250000000.)
+    parser.add_argument("--numtaps", type=int, help='filter order + 1 (default=51)', default=51)
+    parser.add_argument("--r", type=int, help='resistance in ohms (suggested=50)')
+    parser.add_argument("--pmt_hv", type=int, help='voltage of PMT (V) (suggested=1800)')
+    parser.add_argument("--gain", type=int, help='gain of PMT (suggested=1e7)')
+    parser.add_argument("--offset", type=int, help='offset of pulse generator (suggested=0)')
+    parser.add_argument("--trig_delay", type=float, help='delay of pulse generator trigger (ns) (suggested=9.)')
+    parser.add_argument("--amp", type=float, help='amplitude of pulse generator (V) (suggested=3.5)')
+    parser.add_argument("--band", type=str, help='bandwidth of oscilloscope (Hz)')
+    parser.add_argument("--nfilter", type=float, help='noise filter on oscilloscope (bits)')
+    parser.add_argument("--info_file", type=str, help='path to d1 info file')
+    args = parser.parse_args()
+
+    if not args.info_file:
+        if not (args.date or args.date_time or args.fil_band or args.fsps or args.baseline or args.r or args.pmt_hv or
+                args.gain or args.offset or args.trig_delay or args.amp or args.band or args.nfilter):
+            print('Error: Must provide an info file or all other arguments')
+        else:
+            p2(args.start, args.end, args.date, args.date_time, args.fil_band, args.nhdr, args.fsps, args.fc,
+               args.numtaps, args.r, args.pmt_hv, args.gain, args.offset, args.trig_delay, args.amp,
+               args.band, args.nfilter)
+    else:
+        myfile = open(args.info_file, 'r')
+        csv_reader = csv.reader(myfile)
+        info_array = np.array([])
+        path_array = np.array([])
+        for row in csv_reader:
+            info_array = np.append(info_array, row[1])
+        i_date_time = info_array[1]
+        i_path = info_array[3]
+        i_pmt_hv = int(info_array[4])
+        i_gain = int(float(info_array[5]))
+        i_offset = int(info_array[6])
+        i_trig_delay = float(info_array[7])
+        i_amp = float(info_array[8])
+        i_fsps = float(info_array[9])
+        i_band = info_array[10]
+        i_nfilter = float(info_array[11])
+        i_r = int(info_array[12])
+        a, b, c, d, e, fol, f, i_fil_band, g = i_path.split('/')
+        i_date, watch, spe = fol.split('_')
+        i_date = int(i_date)
+
+        p2(args.start, args.end, i_date, i_date_time, i_fil_band, args.nhdr, i_fsps, args.fc, args.numtaps, i_r,
+           i_pmt_hv, i_gain, i_offset, i_trig_delay, i_amp, i_band, i_nfilter)
+
+        myfile.close()
